dd0f24465c49eb85f69e98eceef4357c3e0a0da7
eliburgi <elias.burgstaller@gmx.at>
2020-03-03 09:35:25 +0100

    added shifting tab bar to home page

--- lib/pages/home/layouts/mobile.dart
+++ lib/pages/home/layouts/mobile.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shifting_tabbar/shifting_tabbar.dart';
 
 class HomePageLayoutMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home Page Mobile'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: ShiftingTabBar(
+          tabs: [
+            ShiftingTab(
+              text: 'Item A',
+              icon: Icon(Icons.weekend),
+            ),
+            ShiftingTab(
+              text: 'Item B',
+              icon: Icon(Icons.whatshot),
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text('Item A')),
+            Center(child: Text('Item B')),
+          ],
+        ),
       ),
     );
   }
